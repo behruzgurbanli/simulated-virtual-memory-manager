abe7adf2df38289e0dce0cdc574ecd87fce66d21
apricityyyy <inovruzova@DESKTOP-51EHSG3>
2024-04-27 21:30:56 +0400

    Changed frame numbers and the structure of page table entry for the implementation of second chance algorithm

---
+++ extra_1_program/types_constants.h
@@ -0,0 +1,27 @@
+#ifndef TYPES_CONSTANTS_H
+#define TYPES_CONSTANTS_H
+
+#include <stdbool.h>
+
+#define TLB_SIZE 16
+#define PAGE_SIZE 256
+#define FRAME_SIZE 256 
+#define NUM_FRAMES 64 // memory_size divided by page size
+#define MAX_PAGES NUM_FRAMES
+
+
+typedef struct {
+    unsigned int page_number;
+    unsigned int frame_number;
+    bool valid;
+} TLBEntry;
+
+typedef struct {
+    unsigned int frame_number;
+    bool valid;
+    bool referenced;  // adding R bit for tracking usage of pages
+} PageTableEntry;
+
+extern unsigned char physical_memory[NUM_FRAMES][FRAME_SIZE];
+
+#endif
