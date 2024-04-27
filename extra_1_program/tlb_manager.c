ef496408ccb22d1ec2c0f11bf07c4bf0a2fe9ee4
apricityyyy <inovruzova@DESKTOP-51EHSG3>
2024-04-27 22:26:11 +0400

    First implementation of the second chance algorithm is done

---
+++ extra_1_program/tlb_manager.c
@@ -0,0 +1,26 @@
+#include "types_constants.h"
+
+int tlb_next_index = 0;
+void update_TLB(TLBEntry *tlb, unsigned int page_number, unsigned int frame_number) {
+    for (int i = 0; i < TLB_SIZE; i++) {
+        if (tlb[i].valid && tlb[i].page_number == page_number) {
+            tlb[i].frame_number = frame_number;
+            return;
+        }
+    }
+
+    tlb[tlb_next_index].page_number = page_number;
+    tlb[tlb_next_index].frame_number = frame_number;
+    tlb[tlb_next_index].valid = true;
+    tlb_next_index = (tlb_next_index + 1) % TLB_SIZE;
+}
+
+int search_TLB(const TLBEntry *tlb, unsigned int page_number, int *total_tlb_hits) {
+    for (int i = 0; i < TLB_SIZE; i++) {
+        if (tlb[i].valid && tlb[i].page_number == page_number) {
+            (*total_tlb_hits)++;
+            return tlb[i].frame_number;
+        }
+    }
+    return -1;
+}
